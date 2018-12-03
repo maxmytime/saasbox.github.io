623752a1917f29eda33ea88d433a07f0498f066c
maxmytime <42465581+maxmytime@users.noreply.github.com>
2018-12-03 12:26:11 +0700

    new

--- js/common.js
+++ js/common.js
@@ -27,4 +27,19 @@ $(function() {
           return false;
       });
 
+
+
 });
+
+
+    (function(){ var widget_id = 'zn1XbmAxrT';
+    var d=document;var w=window;function l(){var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = '//code.jivosite.com/script/widget/'+widget_id;
+    var ss = document.getElementsByTagName('script')[0];
+    ss.parentNode.insertBefore(s, ss);}
+    if(d.readyState=='complete'){l();}
+    else{if(w.attachEvent){w.attachEvent('onload',l);}
+    else{w.addEventListener('load',l,false);
+    }}})();
